ed2d257c8a6d0214692e1ac48a4d3f15465391b5
Tjepp <t.bj@outlook.dk>
2018-12-04 17:37:53 +0100

    Added button

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,12 +2,12 @@ import styled from 'styled-components'
 import React from 'react';
 
 const Button = ({name}) => (
-    <div>{`Hi ${name}`}</div>
+    <StyledButton>{`Hi ${name}`}</StyledButton>
 );
 
-export default Header;
+export default Button;
 
-const Button = styled.button`
+const StyledButton = styled.button`
   background: transparent;
   border-radius: 3px;
   border: 2px solid palevioletred;
